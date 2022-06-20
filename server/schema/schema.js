7771ead040ad75b29575c3cbc52ad3c55dfd1a1e
Antonio <antoniosansotta@gmail.com>
2022-06-20 14:56:57 -0400

    updated gitignore

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,7 +7,8 @@ const {
     GraphQLID, 
     GraphQLString, 
     GraphQLSchema, 
-    GraphQLList 
+    GraphQLList,
+    GraphQLNonNull
 } = require('graphql')
 
 // Client Type
@@ -78,6 +79,37 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+// Mutations
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addClient: {
+            type: ClientType,
+            args: {
+                name: {
+                    type: GraphQLNonNull(GraphQLString)
+                },
+                email: {
+                    type: GraphQLNonNull(GraphQLString)
+                },
+                phone: {
+                    type: GraphQLNonNull(GraphQLString)
+                },
+            },
+            resolve(parent, args) {
+                const client = new Client({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone,
+                })
+
+                return client.save()
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 })
